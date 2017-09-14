001c85f7f6d05e7b5d84ed4db8ce122bf513f5c5
Brittany Storoz <brittanystoroz@gmail.com>
2017-09-14 08:21:17 -0600

    Rename randomTip

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 fetch('tips.json')
   .then(response => response.json())
   .then(tips => {
-    let randomTip = tips[Math.floor(Math.random() * tips.length)];
+    let tipOfTheDay = tips[Math.floor(Math.random() * tips.length)];
 
     let tipCategoryElem = document.getElementById('tip-category');
     let tipTextElem = document.getElementById('tip-text');
 
     // Update category text and class
-    tipCategoryElem.textContent = randomTip.tool;
-    tipCategoryElem.classList.add(randomTip.toolSlug);
+    tipCategoryElem.textContent = tipOfTheDay.tool;
+    tipCategoryElem.classList.add(tipOfTheDay.toolSlug);
 
     // Update tip text and add source link if applicable
-    let tipText = randomTip.text.replace(/\[/g, '<code>').replace(/\]/g, '</code>');
+    let tipText = tipOfTheDay.text.replace(/\[/g, '<code>').replace(/\]/g, '</code>');
     tipTextElem.innerHTML = tipText;
 
-    if (randomTip.source) {
+    if (tipOfTheDay.source) {
       let learnMore = document.createElement('a');
-      learnMore.href = randomTip.source;
+      learnMore.href = tipOfTheDay.source;
       learnMore.classList.add('learn-more');
       learnMore.textContent = 'Learn More';
       tipTextElem.appendChild(learnMore);
